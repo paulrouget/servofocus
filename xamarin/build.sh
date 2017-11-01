ecbcf2ace43444e20d0f0142fcfc424e050868ab
Paul Rouget <me@paulrouget.com>
2017-11-01 04:09:50 +0100

    improve script

--- xamarin/build.sh
+++ xamarin/build.sh
@@ -1,20 +1,21 @@
 #!/bin/sh
 
-
-# msbuild servofocus.sln /t:Clean,Build
-msbuild  /t:Clean,Build,PackageForAndroid,SignAndroidPackage ./Servofocus.Android/Servofocus.Android.csproj
+pkg="com.browser.servofocus"
 
 PATH=/Users/paul/Library/Android/sdk//platform-tools/:/Users/paul/Library/Android/sdk//build-tools/25.0.3/:$PATH
-
-apk=./Servofocus.Android/bin/Debug/com.browser.servofocus-Signed.apk
-
-pkg=$(aapt dump badging $apk|awk -F" " '/package/ {print $2}'|awk -F"'" '/name=/ {print $2}')
-act=$(aapt dump badging $apk|awk -F" " '/launchable-activity/ {print $2}'|awk -F"'" '/name=/ {print $2}')
-
 echo "stopping"
 adb shell am force-stop $pkg
+
+msbuild \
+  /t:Clean,Build,PackageForAndroid,SignAndroidPackage,Install \
+  /p:Configuration=Release /p:AdbTarget="-s CB5A2AS54G" \
+  ./Servofocus.Android/Servofocus.Android.csproj
+
+apk=./Servofocus.Android/bin/Release/com.browser.servofocus-Signed.apk
+
+act=$(aapt dump badging $apk|awk -F" " '/launchable-activity/ {print $2}'|awk -F"'" '/name=/ {print $2}')
+
 echo "installing"
 adb install -r $apk
 echo "starting"
 adb shell am start -n $pkg/$act
-adb logcat
