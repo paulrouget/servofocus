066e18dc950af704e38e2c482e7f1f6ead8569ee
Paul Rouget <me@paulrouget.com>
2017-11-15 10:15:08 +0100

    animations

--- xamarin/Servofocus/ServofocusPage.xaml.cs
+++ xamarin/Servofocus/ServofocusPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using Xamarin.Forms;
 using System.Diagnostics;
+using System;
 
 namespace Servofocus
 {
@@ -19,29 +20,65 @@ namespace Servofocus
 
         void Initialize()
         {
-            ServoView.Servo.SetUrlCallback(url => Device.BeginInvokeOnMainThread(() => {
+            this.HideServo();
+ 
+            ServoView.Servo.SetUrlCallback(url => Device.BeginInvokeOnMainThread(() =>
+            {
                 UrlField.Text = url;
             }));
 
-            ServoView.Servo.SetTitleCallback(title => Device.BeginInvokeOnMainThread(() => {
+            ServoView.Servo.SetTitleCallback(title => Device.BeginInvokeOnMainThread(() =>
+            {
                 // FIXME
             }));
 
-            ServoView.Servo.SetHistoryCallback((back, fwd) => Device.BeginInvokeOnMainThread(() => {
+            ServoView.Servo.SetHistoryCallback((back, fwd) => Device.BeginInvokeOnMainThread(() =>
+            {
                 // FIXME
             }));
 
-            ServoView.Servo.SetLoadStartedCallback(() => Device.BeginInvokeOnMainThread(() => {
+            ServoView.Servo.SetLoadStartedCallback(() => Device.BeginInvokeOnMainThread(() =>
+            {
                 // FIXME
+
             }));
 
-            ServoView.Servo.SetLoadEndedCallback(() => Device.BeginInvokeOnMainThread(() => {
+            ServoView.Servo.SetLoadEndedCallback(() => Device.BeginInvokeOnMainThread(() =>
+            {
                 // FIXME
+                ShowServo();
             }));
 
             ServoView.Servo.MeasureUrlHeight = () => (uint)UrlView.Height;
-            
+
             ServoView.Servo.ValidateCallbacks();
+
+
+        }
+
+        void ShowServo()
+        {
+            UrlView.TranslateTo(0, 0, 500, Easing.SpringOut);
+            ServoView.ScaleTo(1, 500, Easing.SpringOut);
+        }
+
+        void HideServo()
+        {
+            UrlView.TranslateTo(0, 200, 500, Easing.SpringIn);
+            ServoView.ScaleTo(0, 500, Easing.SpringIn);
+        }
+
+        void EraseButtonClicked(object sender, EventArgs args)
+        {
+            HideServo();
+        }
+
+        void UrlChanged(object sender, EventArgs args)
+        {
+        }
+
+        void UrlFocused(object sender, EventArgs args)
+        {
         }
 
         protected override void OnDisappearing()
