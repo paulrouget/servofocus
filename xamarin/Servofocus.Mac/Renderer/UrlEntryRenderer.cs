c988113f4058938824056a3494ab0d5e7db83a48
Paul Rouget <me@paulrouget.com>
2017-11-29 09:49:30 +0100

    mac urlview renderer

---
+++ xamarin/Servofocus.Mac/Renderer/UrlEntryRenderer.cs
@@ -0,0 +1,17 @@
+﻿using Servofocus.Mac.Renderer;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.MacOS;
+
+[assembly: ExportRenderer (typeof (Entry), typeof (UrlEntryRenderer))]
+namespace Servofocus.Mac.Renderer
+{
+    public class UrlEntryRenderer : EntryRenderer
+    {
+        protected override void OnElementChanged (ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged (e);
+            Control.Bordered = false;
+            Control.FocusRingType = AppKit.NSFocusRingType.None;
+        }
+    }
+}
